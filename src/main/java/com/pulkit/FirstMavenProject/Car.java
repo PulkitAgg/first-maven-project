798eaba243c2a2bebe237ec9f26e9dd095d3b528
Pulkit <pulkit.aggarwal@nagarro.com>
2022-07-28 17:59:38 +0530

    Use Autowire configuration with annotation based configuration

--- src/main/java/com/pulkit/FirstMavenProject/Car.java
+++ src/main/java/com/pulkit/FirstMavenProject/Car.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Car implements Vehicle {
+	
 	@Autowired
 	private Tyre tyre;
 	
--- src/main/java/com/pulkit/FirstMavenProject/Tyre.java
+++ src/main/java/com/pulkit/FirstMavenProject/Tyre.java
@@ -1,13 +1,19 @@
 package com.pulkit.FirstMavenProject;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Tyre {
+	
+	@Value("MRF")
 	private String brand;
 
 	// when used constructor, we can't used property injection using beans
-	public Tyre(String brand) {
-		super();
-		this.brand = brand;
-	}
+//	public Tyre(String brand) {
+//		super();
+//		this.brand = brand;
+//	}
 
 	public String getBrand() {
 		return brand;
