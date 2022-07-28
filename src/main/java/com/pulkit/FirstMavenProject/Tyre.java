eeb66ff9d54ced5cec11bc396fe0b81d42bfa079
Pulkit <pulkit.aggarwal@nagarro.com>
2022-07-28 15:37:18 +0530

    Add constructor injection

--- src/main/java/com/pulkit/FirstMavenProject/Tyre.java
+++ src/main/java/com/pulkit/FirstMavenProject/Tyre.java
@@ -3,6 +3,12 @@ package com.pulkit.FirstMavenProject;
 public class Tyre {
 	private String brand;
 
+	// when used constructor, we can't used property injection using beans
+	public Tyre(String brand) {
+		super();
+		this.brand = brand;
+	}
+
 	public String getBrand() {
 		return brand;
 	}
